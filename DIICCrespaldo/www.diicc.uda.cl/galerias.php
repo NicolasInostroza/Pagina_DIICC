50e9a87f6b29eb800b6db51365cd906ae3237c1e
FrancoAndahur <francoandahur60@gmail.com>
2023-06-21 04:23:46 -0400

    se eliminaron antiguas cards de galerias

--- DIICCrespaldo/www.diicc.uda.cl/galerias.php
+++ DIICCrespaldo/www.diicc.uda.cl/galerias.php
@@ -29,88 +29,8 @@
             </div>
         </div>
         <!-- fin Banner -->
-        <!-- Album Start -->
-        <div class="galeria">
-            <div class="teacher-area pt-150 pb-105">
-                <div class="container">
-                    <div class="row">         
-                        <div class="col-md-3 col-sm-4 col-xs-12">
-                            <div class="single-teacher mb-45">
-                                <div class="single-teacher-img">
-                                    <a title="galeria" href="https://diicc.uda.cl/galeria.php" target="_blank">
-                                    <img src="img/galeria/img1.jpeg" alt="teacher">  
-                                </div>
-                                <div class="single-teacher-content text-center">
-                                    <h2><a>Bienvenida C2023</a></h2>  
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         
-            <!-- Album End -->
-            <!-- Album Start -->
-            <div class="teacher-area pt-150 pb-105">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-3 col-sm-4 col-xs-12">
-                            <div class="single-teacher mb-45">
-                                <div class="single-teacher-img">
-                                    <a title="galeria" href="https://diicc.uda.cl/titulaciones.php" target="_blank">
-                                    <img src="img/titulos/img1.jpg" alt="teacher">  
-                                </div>
-                                <div class="single-teacher-content text-center">
-                                    <h2><a>Titulaciones</a></h2>  
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Album End -->
-
-            <!-- Album Start -->
-            <div class="teacher-area pt-150 pb-105">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-3 col-sm-4 col-xs-12">
-                            <div class="single-teacher mb-45">
-                                <div class="single-teacher-img">
-                                    <a title="galeria" href="https://diicc.uda.cl/actividades.php" target="_blank">
-                                    <img src="img/actividades/img1.jpg" alt="teacher">  
-                                </div>
-                                <div class="single-teacher-content text-center">
-                                    <h2><a>Actividades</a></h2>  
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Album End -->
-
-            <!-- Album Start -->
-            <div class="teacher-area pt-150 pb-105">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-3 col-sm-4 col-xs-12">
-                            <div class="single-teacher mb-45">
-                                <div class="single-teacher-img">
-                                    <a title="galeria" href="https://diicc.uda.cl/clases.php" target="_blank">
-                                    <img src="img/clases/img1.JPG" alt="teacher">  
-                                </div>
-                                <div class="single-teacher-content text-center">
-                                    <h2><a>Clases</a></h2>  
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Album End -->
-
-        </div> <!--fin galeria-->
+        <div id="espacio"></div>
 
         <main class="page-content">
             <div class="cards">
