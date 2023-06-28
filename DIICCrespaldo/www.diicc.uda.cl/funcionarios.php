b11ec5274cbd0347f3e826a33cfb1157b5ce34c7
FrancoAndahur <francoandahur60@gmail.com>
2023-06-28 11:02:21 -0400

    se agregaron nuevas cards para administrativos

--- DIICCrespaldo/www.diicc.uda.cl/funcionarios.php
+++ DIICCrespaldo/www.diicc.uda.cl/funcionarios.php
@@ -55,6 +55,50 @@
                 </div>
             </div>
         </div>
+
+        <!-- nuevas tarjetas de administrativos -->
+        <div id="espacio"></div>
+        <section class="container">
+            <div class="row active-with-click">
+                <?php
+                    $sql = "SELECT * FROM funcionarios WHERE es_academico = 0   ";
+                    $resultado = mysqli_query($conexion, $sql);
+                    $consecutivo = 1;
+                    while ($mostrar = mysqli_fetch_array($resultado)) {
+                ?>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <article class="material-card Teal">
+                        <h3>
+                            <span><a id="enlace" href="academico.php?id=<?php echo $mostrar['id']; ?>"><?php echo ($mostrar['Nombre']); ?></a></span>
+                            <strong>
+                                <?php echo ($mostrar['cargo']);?>
+                            </strong>
+                        </h3>
+                        <div class="mc-content">
+                            <div class="img-container">
+                                <img alt="teacher" src=<?php echo fromroot($file, $mostrar['img_path']);?>>
+                            </div>
+                            <div class="mc-description">
+                                <?php echo ($mostrar['grado_academico']) ?>
+                            </div>
+                        </div>
+                        <a class="mc-btn-action">
+                            <i class="fa fa-bars"></i>
+                        </a>
+                        <div class="mc-footer">
+                            <h5><?php echo ($mostrar['correo']) ?></h5>
+                            <h5>fono: <?php echo ($mostrar['fono']) ?></h5>
+                        </div>
+                    </article>
+                    <div id="espacio"></div>
+                </div>
+                <?php 
+                    $consecutivo++;
+            
+            } 
+            ?>
+            </div>
+        </section>
                     
         
         <!-- FOOTER -->
