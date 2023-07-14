537c11e7d22fd303e45e7f072c6b015d14f2ba0b
JJRG20 <juanjo_resk@live.com>
2023-07-14 17:18:46 -0400

    Regreso a la barra de números inicial

--- DIICCrespaldo/www.diicc.uda.cl/publicaciones.php
+++ DIICCrespaldo/www.diicc.uda.cl/publicaciones.php
@@ -72,44 +72,28 @@
 
                     <?php  }?>
                
-           
             <div class="row" align="center">
                 <div class="col-xs-12">
                     <div class="pagination">
                         <ul>
-                            <?php
-
-                                $page_query = "SELECT * FROM publicaciones ORDER BY fecha DESC";
-                                $page_result = mysqli_query($conexion, $page_query);
-                                $total_records = mysqli_num_rows($page_result);
-                                $total_pages = ceil($total_records/$registro_por_pagina);
-                                $start_loop = $pagina;
-                                $diferencia = $total_pages - $pagina;
-                                if($diferencia <= 12)
-                                {
-                                $start_loop = $total_pages - 12;
+                            <?php 
+                            $total = mysqli_query($conexion, 'SELECT count(*) from publicaciones;');
+                            
+                                if ($page == 0){
+                                    ++$page;
                                 }
-                                $end_loop = $start_loop + 11;
-                                if($pagina > 1)
-                                {
-                                echo "<li><a class='pagina' href='publicaciones.php?pagina=1'>In</a></li>";
-                                echo "<li><a class='pagina' href='publicaciones.php?pagina=".($pagina - 1)."'><</a></li>";
-                                }
-                                for($i=$start_loop; $i<=$end_loop; $i++)
-                                {     
-                                echo "<li><a class='pagina' href='publicaciones.php?pagina=".$i."'>".$i."</a></li>";
-                                }
-                                if($pagina <= $end_loop)
-                                {
-                                echo "<li><a class='pagina' href='publicaciones.php?pagina=".($pagina + 1)."'>></a></li>";
-                                echo "<li><a class='pagina' href='publicaciones.php?pagina=".$total_pages."'>>></a></li>";
+                                    $page = $page + 1;
+                                    echo '<li><a href="publicaciones.php">1</a></li>';
+                                if ($resultado->num_rows != 0){
+                                    echo sprintf('<li><a href="publicaciones.php?page=%d">%d</a></li>', $page, $page);
                                 }
                             
                             ?>
                         </ul>
                     </div>
                 </div>
-            </div>
+            </div><br>
+            
             
 
             
